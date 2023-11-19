e9f94343856a24e62527268097000baad0daacdd
amin jabari <aminjabari.dev@gmail.com>
2023-11-19 19:15:21 +0330

    fix

--- pages/about-us/index.vue
+++ pages/about-us/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+<div>
+  <div class="d-none d-md-block">
     <div class="d-flex justify-center">
       <img src="~/assets/img/about-us-image.png" class="about-us-image" alt="">
     </div>
@@ -59,11 +60,79 @@
     </div>
   </div>
 
+  <!--  mobile-->
+  <div class="d-block d-md-none">
+    <div class="blog-box-yellow-mobile d-flex justify-center align-center">
+      <img width="56" height="56" src="~/assets/img/arrow-up-right-Pr.svg" alt="">
+    </div>
+    <div class="d-flex justify-center" style="    margin-top: 35px;">
+      <img  src="~/assets/img/about-us-image.png" class="about-us-image" alt="">
+    </div>
+    <div class="blog-box-primary-mobile d-flex justify-center align-center">
+      <img  width="36" height="36" src="~/assets/img/arrow-up-right-Yellow.svg" alt="">
+    </div>
+    <div class="d-flex justify-center " style="    margin-top: -211px;" >
+      <v-img :src="require('~/assets/img/EllipseMobile.png')" alt="">
+      </v-img>
+    </div>
+    <div class="main-container" style="margin-top: -275px;">
+      <div class="text-center px-15">
+          <span class="primary--text t18600">
+          درباره ما
+          </span>
+      </div>
+      <div class="text-center mt-2">
+          <span class="primary--text t16600">
+         مسیر از کجا شروع شد و با چه هدفی؟
+          </span>
+      </div>
+
+      <div class="text-center mt-15 ">
+        <span class="primary--text t18600">
+          مقاله های اخیرا منتشر شده
+        </span>
+      </div>
+      <div class="text-right  pt-15" style="    margin-top: -5px;">
+        <span class="primary--text t18600">
+          ایجاد پروژه برای نیروی متخصص
+        </span>
+        <p class="primary--text t18400 lh-40 text-justify mt-10 px-3">
+          لودر یکی از کاربردی‌ترین ماشین‌آلات ساختمانی و عمرانی است. این ماشین که در اندازه‌های مختلف ساخته می‌شود به دلیل عملکرد و انعطاف‌پذیری زیادی که دارد و نیز با کمک تغییر جام می‌تواند بسیاری از کارها را انجام دهد. لودر موارد استفاده بسیاری دارد که برخی از آن‌ها عبارتند از: ایجاد خاکریزها، حفاری زیرزمین بناها، پرکردن خندقها، خاکریزی اطراف لوله‌های کارگذاشته شده در کانال‌ها، بارکردن کامیون‌ها، حمل بتن به محل قالب‌ها و بلند کردن و حمل مصالح ساختمانی. به ماشین لودر می‌توان انواع ملحقات نظیر برف‌روب، کانال‌کن، لوله‌بر، لوله‌گذار و جرثقیل و لیفتراک را نصب کرد و کاربردهای دیگری از آن گرفت.
+        </p>
+        <p class="primary--text t18400 lh-40 text-justify mt-10 px-3">
+          لودر یکی از کاربردی‌ترین ماشین‌آلات ساختمانی و عمرانی است. این ماشین که در اندازه‌های مختلف ساخته می‌شود به دلیل عملکرد و انعطاف‌پذیری زیادی که دارد و نیز با کمک تغییر جام می‌تواند بسیاری از کارها را انجام دهد. لودر موارد استفاده بسیاری دارد که برخی از آن‌ها عبارتند از: ایجاد خاکریزها، حفاری زیرزمین بناها، پرکردن خندقها، خاکریزی اطراف لوله‌های کارگذاشته شده در کانال‌ها، بارکردن کامیون‌ها، حمل بتن به محل قالب‌ها و بلند کردن و حمل مصالح ساختمانی. به ماشین لودر می‌توان انواع ملحقات نظیر برف‌روب، کانال‌کن، لوله‌بر، لوله‌گذار و جرثقیل و لیفتراک را نصب کرد و کاربردهای دیگری از آن گرفت.
+        </p>
+      </div>
+      <v-row justify="space-between" class="mt-15 pt-15">
+        <v-col cols="12">
+          <img width="312" height="246" src="~/assets/img/about-us-service.png">
+        </v-col>
+        <v-col cols="12">
+          <div class="mt-5">
+            <span class="primary--text t30600">
+              درباره <span class="primaryYellow--text">سرویس‌های</span> مسیر
+            </span>
+          </div>
+          <div class=" mt-10">
+            <p class="primary--text t18400 text-justify lh-40 px-3">
+              لودر یکی از کاربردی‌ترین ماشین‌آلات ساختمانی و عمرانی است. این ماشین که در اندازه‌های مختلف ساخته می‌شود به دلیل عملکرد و انعطاف‌پذیری زیادی که دارد و نیز با کمک تغییر جام می‌تواند بسیاری از کارها را انجام دهد. لودر موارد استفاده بسیاری دارد که برخی از آن‌ها عبارتند از
+            </p>
+          </div>
+        </v-col>
+
+      </v-row>
+    </div>
+  </div>
+</div>
+
 
 </template>
 <script>
 
+  import BlogCard from "~/components/Public/BlogCard.vue";
+
   export default {
+    components: {BlogCard},
     layout:'static'
   }
 </script>
---
+++ pages/blog/_id/index.vue
@@ -0,0 +1,248 @@
+<template>
+  <div>
+   <div >
+     <div class="pdp-blog-header-box">
+       <div class="yellow-box d-flex justify-center align-center">
+         <img  src="~/assets/img/arrow-up-right-Pr.svg" alt="">
+       </div>
+       <div class="white-box d-none d-md-flex justify-center align-center">
+         <img  src="~/assets/img/arrow-up-right-Yellow.svg" alt="">
+       </div>
+       <div class="primary-box d-flex justify-center align-center">
+         <img   src="~/assets/img/arrow-up-right-Yellow.svg" alt="">
+       </div>
+     </div>
+     <div class="main-container margin-lg">
+       <div class="pr-10 d-none d-md-block">
+         <h1 class="primaryYellow--text t30600 ">ایجاد پروژه برای نیروی متخصص</h1>
+
+       </div>
+       <div class="d-block d-md-none text-center">
+         <h1 class="primaryYellow--text t18600 ">ایجاد پروژه برای نیروی متخصص</h1>
+
+       </div>
+
+       <div class="pr-10 mt-5 d-none d-md-block">
+         <span class="white--text t24600">ایجاد پروژه برای نیروی متخصص</span>
+       </div>
+       <div class=" mt-5 d-block d-md-none text-center">
+         <span class="white--text t16600">ایجاد پروژه برای نیروی متخصص</span>
+       </div>
+       <div class="d-none d-md-flex justify-start pr-10 my-5">
+        <span class="primaryYellow--text t18400">
+          نویسنده: بهار قاسمی
+        </span>
+         <span class="primaryYellow--text t18400 mr-8">
+          تاریخ: ۱۴۰۲/۱۲/۲۴
+        </span>
+       </div>
+       <div class="d-block d-md-none my-5">
+         <div class="text-center">
+            <span class="white--text t16400">
+            نویسنده: بهار قاسمی
+          </span>
+         </div>
+         <div class="text-center mt-3">
+            <span class="white--text t16400">
+            تاریخ: ۱۴۰۲/۱۲/۲۴
+          </span>
+         </div>
+       </div>
+       <div class="d-flex justify-center ">
+
+         <img class="blog-banner-img" src="~/assets/img/blogBanner.png" >
+       </div>
+
+       <div class="mt-10 px-15 d-none d-md-block">
+        <span class="primary--text t30600 ">
+          ایجاد پروژه برای نیروی متخصص
+        </span>
+
+
+         <p class="mt-10 lh-40 text-justify t18400">
+           لودر یکی از کاربردی‌ترین ماشین‌آلات ساختمانی و عمرانی است. این ماشین که در اندازه‌های مختلف ساخته می‌شود به دلیل عملکرد و انعطاف‌پذیری زیادی که دارد و نیز با کمک تغییر جام می‌تواند بسیاری از کارها را انجام دهد. لودر موارد استفاده بسیاری دارد که برخی از آن‌ها عبارتند از: ایجاد خاکریزها، حفاری زیرزمین بناها، پرکردن خندقها، خاکریزی اطراف لوله‌های کارگذاشته شده در کانال‌ها، بارکردن کامیون‌ها، حمل بتن به محل قالب‌ها و بلند کردن و حمل مصالح ساختمانی. به ماشین لودر می‌توان انواع ملحقات نظیر برف‌روب، کانال‌کن، لوله‌بر، لوله‌گذار و جرثقیل و لیفتراک را نصب کرد و کاربردهای دیگری از آن گرفت.
+         </p>
+       </div>
+       <div class="mt-10  d-block d-md-none">
+        <span class="primary--text t18600 ">
+          ایجاد پروژه برای نیروی متخصص
+        </span>
+
+
+         <p class="mt-10 lh-40 px-5 text-justify t18400">
+           لودر یکی از کاربردی‌ترین ماشین‌آلات ساختمانی و عمرانی است. این ماشین که در اندازه‌های مختلف ساخته می‌شود به دلیل عملکرد و انعطاف‌پذیری زیادی که دارد و نیز با کمک تغییر جام می‌تواند بسیاری از کارها را انجام دهد. لودر موارد استفاده بسیاری دارد که برخی از آن‌ها عبارتند از: ایجاد خاکریزها، حفاری زیرزمین بناها، پرکردن خندقها، خاکریزی اطراف لوله‌های کارگذاشته شده در کانال‌ها، بارکردن کامیون‌ها، حمل بتن به محل قالب‌ها و بلند کردن و حمل مصالح ساختمانی. به ماشین لودر می‌توان انواع ملحقات نظیر برف‌روب، کانال‌کن، لوله‌بر، لوله‌گذار و جرثقیل و لیفتراک را نصب کرد و کاربردهای دیگری از آن گرفت.
+         </p>
+       </div>
+       <div class="pdp-blog-status-box mt-15 px-5 d-md-flex d-none justify-space-between align-center">
+         <div class="d-flex justify-start align-center">
+           <div class="chip mx-2">
+            <span class="primary--text t18400">
+              سنگ های گرانبها
+            </span>
+           </div>
+           <div class="chip mx-2">
+            <span class="primary--text t18400">
+              سنگ های گرانبها
+            </span>
+           </div>
+         </div>
+         <div>
+           <div class="chip mx-2">
+
+            <span class="primary--text t18400">
+              Copy Link
+            </span>
+             <span class="mr-2">
+              <img src="~/assets/img/copy.svg">
+            </span>
+           </div>
+         </div>
+       </div>
+       <div class="pdp-blog-status-box mt-15 pa-5 d-md-none d-block ">
+         <div class="chip my-2">
+            <span class="primary--text t18400">
+              سنگ های گرانبها
+            </span>
+         </div>
+         <div class="chip my-2">
+            <span class="primary--text t18400">
+              سنگ های گرانبها
+            </span>
+         </div>
+
+       </div>
+       <div class="d-md-none d-block text-center pa-15">
+         <div class="chip-mobile  mx-2">
+
+            <span class="primary--text t18400">
+              Copy Link
+            </span>
+           <span class="mr-2">
+              <img src="~/assets/img/copy.svg">
+            </span>
+         </div>
+       </div>
+     </div>
+   </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  layout:'userProfile'
+}
+</script>
+<style>
+@media only screen and (max-width: 500px) {
+  .chip-mobile{
+    width: 176px;
+    height: 51px;
+    border-radius: 24.5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #DAF1FF;
+  }
+  .blog-banner-img{
+    width: 289px;
+    height: 194px;
+    margin-top: 48px;
+  }
+  .margin-lg{
+    margin-top: -324px  !important;
+  }
+  .pdp-blog-header-box{
+    height: 271px !important;
+    border-radius: 0px 0px 30px 30px !important;
+  }
+  .pdp-blog-header-box>.yellow-box{
+    border-radius: 10px !important;
+    z-index: 1;
+    position: absolute;
+    top: 123px!important;
+    left: 51px!important;
+    width: 80px !important;
+    height: 76px !important;
+    background: #FFC51E;
+  }
+  .pdp-blog-header-box>.yellow-box img{
+    width: 56px;
+    height: 55px;
+  }
+  .pdp-blog-header-box>.primary-box {
+    border-radius: 10px !important;
+    z-index: 1;
+    position: absolute;
+    top: 197px !important;
+    right: 38px !important;
+    width: 52px !important;
+    height: 49px !important;
+    background: #0C385F;
+  }
+  .pdp-blog-header-box>.primary-box img {
+    width: 36px ;
+    height: 36px;
+  }
+
+}
+
+.pdp-blog-header-box{
+  margin-top: -2px;
+  position: relative;
+  height: 450px;
+  border-radius: 0px 0px 100px 100px;
+  background: #0C385F;
+}
+.pdp-blog-header-box>.yellow-box{
+  border-radius: 50px;
+
+  z-index: 1;
+  position: absolute;
+  top: -33px;
+  left: 254px;
+  width: 161px;
+  height: 152px;
+  background: #FFC51E;
+}
+.pdp-blog-header-box>.primary-box {
+  border-radius: 20px;
+  z-index: 1;
+  position: absolute;
+  top: 175px;
+  right: 260px;
+  width: 78px;
+  height: 73px;
+  background: #0C385F;
+}
+
+.pdp-blog-header-box>.white-box{
+  border-radius: 20px;
+
+  z-index: 1;
+  position: absolute;
+  top: -153px;
+  left: 145px;
+  width: 78px;
+  height: 73px;
+  background: #fff;
+}
+.pdp-blog-status-box{
+  min-height: 105px;
+  background: #DAF1FF;
+  border-radius: 20px;
+}
+.pdp-blog-status-box .chip{
+  height: 51px;
+  border-radius: 24.5px;
+  min-width: 176px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background:#fff;
+}
+
+.margin-lg{
+  margin-top: -550px;
+}
+</style>
